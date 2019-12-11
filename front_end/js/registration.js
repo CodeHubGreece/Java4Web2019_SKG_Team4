e0e59fca4162d8f3699a21c71b355d585d90d934
iliasgdgr <papadopoulos.ilias.gr@gmail.com>
2019-12-11 19:16:49 +0200

    update

---
+++ front_end/js/registration.js
@@ -0,0 +1,13 @@
+function registration(){
+    function logIn() {
+        let name = document.getElementById("firstname").value;
+        let lastname = document.getElementById("lastname").value;
+        let amka = document.getElementById("amka").value;
+        let email = document.getElementById("email").value;
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        let phone = document.getElementById("phone").value;
+        
+
+    }
+}
