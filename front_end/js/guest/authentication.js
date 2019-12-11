709913c4a54ababad661f2ab13ecb555ff59e53b
nasibill <thanasibilero@gmail.com>
2019-12-11 20:58:27 +0200

    ADD

--- front_end/js/guest/authentication.js
+++ front_end/js/guest/authentication.js
@@ -40,6 +40,41 @@ function logOut(){
     window.location.replace("Login.html");
 }
 
-function register() {
-    // TODO:
+function register(firstnameElement,lastnameElement,amkaElement,emailElement,usernameElement,passwordElement,phoneElement) {
+    let firstname = firstnameElement && firstnameElement.value ? firstnameElement.value : "";
+    let lastname = lastnameElement && lastnameElement.value ? lastnameElement.value : "";
+    let amka = amkaElement && amkaElement.value ? amkaElement.value : "";
+    let email = emailElement && emailElement.value ? emailElement.value : "";
+    let username = usernameElement && usernameElement.value ? usernameElement.value : "";
+    let password = passwordElement && passwordElement.value ? passwordElement.value : "";
+    let phone = phoneElement && phoneElement.value ? phoneElement.value : "";
+
+
+    const ROOT_PATH = "http://localhost:8080";
+    var fd = new FormData();
+    fd.append( 'firstname', firstname);
+    fd.append( 'lastname', lastname);
+    fd.append( 'amka', amka);
+    fd.append( 'email', email);
+    fd.append( 'username', username);
+    fd.append( 'password', password);
+    fd.append( 'phone', phone);
+    
+    $.ajax({
+      url: ROOT_PATH + '/api/register',
+      data: fd,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      success: function(data){
+        sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, username);
+        window.location.replace(ROOT_PATH + "/pages/user/register.html");
+      },
+      statusCode: {
+        401 : function() {
+                alert("Invalid username or password!");
+            }
+        }
+    });
+
 }
