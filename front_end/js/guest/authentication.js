3818a2d84342a2c57798c7b4bba18fb6ffab2a3f
nasibill <thanasibilero@gmail.com>
2019-12-13 20:25:54 +0200

    Regist

--- front_end/js/guest/authentication.js
+++ front_end/js/guest/authentication.js
@@ -40,47 +40,35 @@ function logOut(){
     window.location.replace("Login.html");
 }
 
-function register(citizen_nameElement,citizen_lastnameElement,amkaElement,citizen_emailElement,usernameElement,citizen_passwordElement,citizen_phoneElement) {
+function register() {
     
-    
-    let citizen_name = citizen_nameElement && citizen_nameElement.value ? citizen_nameElement.value : "";
-    let citizen_lastname = citizen_lastnameElement && citizen_lastnameElement.value ? citizen_lastnameElement.value : "";
-    let amka = amkaElement && amkaElement.value ? amkaElement.value : "";
-    let citizen_email = citizen_emailElement && citizen_emailElement.value ? citizen_emailElement.value : "";
-    let username = usernameElement && usernameElement.value ? usernameElement.value : "";
-    let citizen_password = citizen_passwordElement && citizen_passwordElement.value ? citizen_passwordElement.value : "";
-    let citizen_phone = citizen_phoneElement && citizen_phoneElement.value ? citizen_phoneElement.value : "";
+  let username = document.getElementById('username').value;
+  let citizen_password = document.getElementById('citizen_password').value;
+  let citizen_lastname = document.getElementById('citizen_lastname').value;
+  let citizen_name = document.getElementById('citizen_name').value;
+  let citizen_email = document.getElementById('citizen_email').value;
+  let amka = document.getElementById('amka').value;
+  let citizen_phone = document.getElementById('citizen_phone').value;
+             $.ajax({
+                      url: ROOT_PATH + '/registration',
+                      type: 'POST',
+                       dataType: 'json',
+                       data: JSON.stringify({
+                       username:username,
+                       citizen_password:citizen_password,
+                       citizen_firstname:citizen_lastname,
+                       citizen_name:citizen_name,
+                       citizen_phone:citizen_phone,
+                       citizen_email:citizen_email,
+                       amka:amka
+                  }),
+                  contentType: 'application/json',
+                  success: function (result) {
+                      alert("Data Saved: " + JSON.stringify(result));
+                  },
+                  error: function (xhr, resp, text) {
+                      alert("NOT Saved: " + text);
+                  }
+              });
+}
 
-    if (username.length>5  && citizen_password.length>5 && citizen_name.length>5 && citizen_lastname.length>5 && amka.length>10 && citizen_phone.length>9){
-    const ROOT_PATH = "http://localhost:8080";
-    var fd = new FormData();
-    fd.append( 'citizen_name', citizen_name);
-    fd.append( 'citizen_lastname', citizen_lastname);
-    fd.append( 'amka', amka);
-    fd.append( 'citizen_email', citizen_email);
-    fd.append( 'username', username);
-    fd.append( 'citizen_password', citizen_password);
-    fd.append( 'citizen_phone', citizen_phone);
-    
-    $.ajax({
-      url: ROOT_PATH + '/api/register',
-      data: fd,
-      processData: false,
-      contentType: false,
-      type: 'POST',
-      success: function(data){
-        sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, username);
-        window.location.replace(ROOT_PATH + "/register.html");
-      },
-      statusCode: {
-        401 : function() {
-                alert("Invalid username or password!");
-            }
-        }
-    });
-
-}
-else {
-  alert("Wrong");
-}
-}
